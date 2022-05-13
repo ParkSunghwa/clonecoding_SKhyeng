9538359b708abbd33f291cf1e026bbeb6c5620cc
ParkSunghwa <mtreeferry@gmail.com>
2022-05-13 15:39:00 +0900

    220513

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
 
+    // AOS operation
+    AOS.init();
+
+
 
     // .wrap_topbar
     let lastScrollTop = 0,
     delta = 15;
-    // let lastTime = new Date().getTime;
 
     $(window).scroll(function(){
 
@@ -22,38 +25,24 @@ $(document).ready(function(){
             });
         }
 
-
-        // let recentTime = new Date().getTime,
-        // changeScroll = Math.abs(lastScrollTop - recentScrollTop),
-        // changeTime = Math.abs(recentTime - lastTime);
-        // let recentSpeed = changeScroll / changeTime;
-        // console.log(recentTime);
-        // console.log(recentSpeed);
-        // if(recentSpeed > 0){
-        //     $(".wrap_sideindex").css({
-        //         "right" : "-111px",
-        //         "opacity" : "0",
-        //     });
-        // } else{
-        //     $(".wrap_sideindex").css({
-        //         "right" : "0",
-        //         "opacity" : "1",
-        //     });
-        // }
-
-        // lastTime = recentTime;
-
-
-
         lastScrollTop = recentScrollTop;
-
-
 
     });
 
+    // .wrap_mainvisual & .wrap_section01
 
+    $(".wrap_mainvisual > .mainvisual > .box_layout_title_mainvisual > .box_title_mainvisual").click(function(){
 
+        let index = $(this).index();
 
+        $(this).addClass("active");
+        $(".wrap_mainvisual > .mainvisual > .bg_mainvisual").eq(index).addClass("active");
+        $(".wrap_section01 > .section01").eq(index).addClass("active")
+
+        $(this).siblings().removeClass("active");
+        $(".wrap_mainvisual > .mainvisual > .bg_mainvisual").eq(index).siblings().removeClass("active");
+        $(".wrap_section01 > .section01").eq(index).siblings().removeClass("active");
+    });
 
 
 
@@ -62,7 +51,6 @@ $(document).ready(function(){
     $(".wrap_section04 > .section04 > .box_layout_section04 > .box_content > ul > .place").click(function(){
 
         let index = $(this).index();
-        console.log(index);
 
         $(this).addClass("active");
         $(this).children(".box_text").css({
@@ -85,53 +73,33 @@ $(document).ready(function(){
 
     $(window).scroll(function(){
 
-        if($(this).scrollTop() == 0){
+        $(".wrap_sideindex").css({
+            "right" : "-111px",
+            "opacity" : "0",
+        });
+    });
+
+    $.fn.scrollStopped = function(callback) {
+        var that = this, $this = $(that);
+        $this.scroll(function(ev) {
+          clearTimeout($this.data('scrollTimeout'));
+          $this.data('scrollTimeout', setTimeout(callback.bind(that), 250, ev));
+        });
+      };
+       
+    $(window).scrollStopped(function(ev){
+        if(this.scrollTop() < 15){
             $(".wrap_sideindex").css({
                 "right" : "-111px",
                 "opacity" : "0",
             });
-        } else{
+        } else {
             $(".wrap_sideindex").css({
                 "right" : "0",
                 "opacity" : "1",
-            });
+            }); 
         };
-
+                   
     });
 
-    // $.fn.scrollStopped = function(callback) {
-    //     var that = this, $this = $(that);
-    //     $this.scroll(function(ev) {
-    //       clearTimeout($this.data('scrollTimeout'));
-    //       $this.data('scrollTimeout', setTimeout(callback.bind(that), 250, ev));
-    //     });
-    //   };
-       
-    // $(window).scrollStopped(function(ev){
-        
-            
-    // });
-
-    // var lastOffset = $(window).scrollTop();
-    // var lastDate = new Date().getTime();
-    // $(window).scroll(function(e) {
-    //     var delayInMs = e.timeStamp - lastDate;
-    //     var offset = e.target.scrollTop - lastOffset;
-    //     var speedInpxPerMs = offset / delayInMs;
-    //     console.log(speedInpxPerMs);
-    //     lastDate = e.timeStamp;
-    //     lastOffset = e.target.scrollTop;
-    // });
-
-    // if(speedInpxPerMs >= 0){
-    //     $(".wrap_sideindex").css({
-    //         "right" : "-111px",
-    //         "opacity" : "0",
-    //     });
-    // } else{
-    //     $(".wrap_sideindex").css({
-    //         "right" : "0",
-    //         "opacity" : "1",
-    //     });
-    // };
 });
